576e5d027cf9ffa144bb6e33d8999afe2aade721
Mags <maggy123@yahoo.com>
2018-09-24 16:29:20 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,18 @@
-var computerChoices = ["1", "2", "3", "4", "4", "5", "6", "7", "8", "9",];
-var wins = 0;
-var losses = 0;
-var numGuesses = 10;
-var guessedChoices = [];
-var userGuess;
 
-var reset = function () {
-    guessedChoices = [];
-    numGuesses = 10;
+var targetNumber = 50;
+
+$("#number-to-guess").text(targetNumber);
+
+var counter = 0
+
+$(".colorblue").on("click",function(){
+    counter +=10;
+
+});
+
+if(counter === targetNumber){
+
+
 }
 
-document.onkeydown = function (event) {
-
-
-    userGuess = event.key;
-
-    var randomcomputerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    if (userGuess === randomcomputerGuess) {
-        wins++;
-        numGuesses--;
-        guessedChoices.push(userGuess);
-    }
-    else {
-        losses++;
-        numGuesses--;
-        guessedChoices.push(userGuess);
-    }
-
-    if (numGuesses === 0) {
-        reset();
-    }
-
-    
-
-    var html =
-        "<h1> The Crystal Game </h1>" +
-        "<p>Guess what letter I'm thinking of!</p>" +
-        "<p>Wins: " + wins + "</p>" +
-        "<p>Losses: " + losses + "</p>" +
-        "<p>Guesses Left: " + numGuesses + "</p>" +
-        "<p>Your Guesses so far: " + guessedChoices.join(", ") + "</p>";
-
-    document.querySelector("#game").innerHTML = html;
-
-
-
-};
+document.getElementById()
